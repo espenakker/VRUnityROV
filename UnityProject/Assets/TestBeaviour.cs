958fb4d20d282db536981f0caa87b926b866ec7d
erik espenakk <erik.espenakk@cdptech.com>
2017-02-15 15:36:50 +0100

    Initial unity commit

---
+++ UnityProject/Assets/TestBeaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBeaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		WebCamDevice[] devices = WebCamTexture.devices;
+		for( int i = 0 ; i < devices.Length ; i++ )
+			Debug.Log(devices[i].name);  
+		WebCamTexture webcamTexture = new WebCamTexture();
+		webcamTexture.deviceName = devices [1].name;
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3 (0,Mathf.Sin(Time.timeSinceLevelLoad)*2.5f,0);		
+	}
+}
