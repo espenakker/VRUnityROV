dc709a12a9cf97c5d1d34a1689b846c3890112a6
erik espenakk <erik.espenakk@cdptech.com>
2017-03-01 13:16:37 +0100

    Adds steamVR, Change mainscene to include a rough interior.

--- UnityProject/Assets/TestBeaviour.cs
+++ UnityProject/Assets/TestBeaviour.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestBeaviour : MonoBehaviour {
-
+	static string othercam = "";
 	// Use this for initialization
 	void Start() {
 		WebCamDevice[] devices = WebCamTexture.devices;
-		for( int i = 0 ; i < devices.Length ; i++ )
-			Debug.Log(devices[i].name);  
 		WebCamTexture webcamTexture = new WebCamTexture();
-		webcamTexture.deviceName = devices [1].name;
+		for (int i = 0; i < devices.Length; i++) {
+			Debug.Log (devices[i].name);
+			if(devices[i].name.Contains("USB") &&
+				!devices[i].name.Equals(othercam)){
+				webcamTexture.deviceName = devices[i].name;
+				othercam = devices[i].name;
+				break;
+			}
+		}
 		Renderer renderer = GetComponent<Renderer>();
 		renderer.material.mainTexture = webcamTexture;
 		webcamTexture.Play();
@@ -18,6 +24,6 @@ public class TestBeaviour : MonoBehaviour {
 		
 	// Update is called once per frame
 	void Update () {
-		this.transform.position = new Vector3 (0,Mathf.Sin(Time.timeSinceLevelLoad)*2.5f,0);		
+		//this.transform.position = new Vector3 (0,Mathf.Sin(Time.timeSinceLevelLoad)*2.5f,0);		
 	}
 }
